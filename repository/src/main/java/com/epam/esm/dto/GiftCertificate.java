ddaae4b83b74304d3b9d7e74283dd518ec1bfc7a
Peachex <klevolex@gmail.com>
2021-06-05 15:15:31 +0300

    add dto

---
+++ repository/src/main/java/com/epam/esm/dto/GiftCertificate.java
@@ -0,0 +1,128 @@
+package com.epam.esm.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+public class GiftCertificate {
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private int duration;
+    private LocalDateTime createDate;
+    private LocalDateTime lastUpdateDate;
+    private List<Tag> tags;
+
+    public GiftCertificate() {
+
+    }
+
+    public GiftCertificate(String name, String description, double price, int duration, LocalDateTime createDate,
+                           LocalDateTime lastUpdateDate, List<Tag> tags) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.duration = duration;
+        this.createDate = createDate;
+        this.lastUpdateDate = lastUpdateDate;
+        this.tags = tags;
+    }
+
+    public GiftCertificate(long id, String name, String description, double price, int duration,
+                           LocalDateTime createDate, LocalDateTime lastUpdateDate, List<Tag> tags) {
+        this(name, description, price, duration, createDate, lastUpdateDate, tags);
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(LocalDateTime lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || o.getClass() != this.getClass()) {
+            return false;
+        }
+        GiftCertificate that = (GiftCertificate) o;
+        return (id == that.id && Double.compare(that.price, price) == 0 && duration == that.duration &&
+                name.equals(that.name) && description.equals(that.description) && createDate.equals(that.createDate) &&
+                Objects.equals(lastUpdateDate, that.lastUpdateDate) && tags.equals(that.tags));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, price, duration, createDate, lastUpdateDate, tags);
+    }
+
+    @Override
+    public String toString() {
+        return ("id = " + id +
+                "\nname = " + name +
+                "\ndescription = " + description +
+                "\nprice = " + price +
+                "\nduration = " + duration +
+                "\ncreateDate = " + createDate +
+                "\nlastUpdateDate = " + lastUpdateDate +
+                "\ntags = " + tags);
+    }
+}
---
+++ repository/src/main/java/com/epam/esm/dto/Tag.java
@@ -0,0 +1,55 @@
+package com.epam.esm.dto;
+
+import java.util.Objects;
+
+public class Tag {
+    private long id;
+    private String name;
+
+    public Tag() {
+    }
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Tag(long id, String name) {
+        this(name);
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || o.getClass() != this.getClass()) {
+            return false;
+        }
+        Tag tag = (Tag) o;
+        return (id == tag.id && name.equals(tag.name));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return ("id = " + id +
+                "\nname = " + name);
+    }
+}
