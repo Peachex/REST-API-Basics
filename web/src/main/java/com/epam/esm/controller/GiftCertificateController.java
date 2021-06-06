81784278301479a8f5c71b1b367c1aa6d0f2e53e
Peachex <klevolex@gmail.com>
2021-06-07 00:31:43 +0300

    delete unnecessary imports

--- web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
+++ web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -3,15 +3,10 @@ package com.epam.esm.controller;
 import com.epam.esm.dto.GiftCertificate;
 import com.epam.esm.service.GiftCertificateService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class GiftCertificateController {
@@ -24,7 +19,6 @@ public class GiftCertificateController {
 
     @GetMapping("/certificates")
     public List<GiftCertificate> findCertificatesWithTags() {
-        System.out.println(service.findCertificatesWithTags());
         return service.findCertificatesWithTags();
     }
 }
