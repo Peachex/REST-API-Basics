a06f5b51cf7679d74be1bd0dea2b1c0597953d4d
Peachex <klevolex@gmail.com>
2021-06-10 13:10:21 +0300

    add function

--- service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
+++ service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
@@ -6,16 +6,29 @@ import com.epam.esm.service.GiftCertificateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.epam.esm.validator.GiftCertificateValidator.isGiftCertificateCreationFormValid;
+
 @Service
-public class GiftCertificateServiceImpl implements GiftCertificateService {
+public class GiftCertificateServiceImpl implements GiftCertificateService<GiftCertificate> {
     private final GiftCertificateDao<GiftCertificate> dao;
 
     @Autowired
     public GiftCertificateServiceImpl(GiftCertificateDao<GiftCertificate> dao) {
         this.dao = dao;
+    }
+
+    @Override
+    public boolean insert(GiftCertificate giftCertificate) {
+        boolean result = false;
+        if (isGiftCertificateCreationFormValid(giftCertificate)) {
+            giftCertificate.setCreateDate(LocalDateTime.now());
+            result = dao.insert(giftCertificate);
+        }
+        return result;
     }
 
     @Override
