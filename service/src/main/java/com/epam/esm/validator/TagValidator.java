3a12cc4990ddc6c0291cf526c2912d033c3b74ed
Peachex <klevolex@gmail.com>
2021-06-10 19:56:03 +0300

    update regex pattern

--- service/src/main/java/com/epam/esm/validator/TagValidator.java
+++ service/src/main/java/com/epam/esm/validator/TagValidator.java
@@ -3,7 +3,7 @@ package com.epam.esm.validator;
 import java.util.regex.Pattern;
 
 public final class TagValidator {
-    private static final Pattern NAME_PATTERN = Pattern.compile("[a-zA-Zа-яА-Я]{1,256}");
+    private static final Pattern NAME_PATTERN = Pattern.compile("[А-Яа-я\\w\\s\\p{Punct}]{1,256}");
 
     private TagValidator() {
     }
