e3346ea2792039a2d65cd25066e1affa3af20615
Peachex <klevolex@gmail.com>
2021-06-10 19:57:28 +0300

    add handler

--- web/src/main/java/com/epam/esm/handler/InvalidFieldExceptionHandler.java
+++ web/src/main/java/com/epam/esm/handler/InvalidFieldExceptionHandler.java
@@ -1,16 +1,16 @@
 package com.epam.esm.handler;
 
 import com.epam.esm.exception.ErrorResponse;
-import com.epam.esm.exception.ResourceNotFoundException;
+import com.epam.esm.exception.InvalidFieldException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ResourceNotFoundExceptionsHandler {
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public final ResponseEntity<ErrorResponse> handleRuntimeExceptions(ResourceNotFoundException e) {
+public class InvalidFieldExceptionHandler {
+    @ExceptionHandler(InvalidFieldException.class)
+    public final ResponseEntity<ErrorResponse> handleRuntimeExceptions(InvalidFieldException e) {
         return new ResponseEntity<>(new ErrorResponse(e.getLocalizedMessage(), e.getErrorCode()),
-                ResourceNotFoundException.getStatus());
+                InvalidFieldException.getStatus());
     }
 }
