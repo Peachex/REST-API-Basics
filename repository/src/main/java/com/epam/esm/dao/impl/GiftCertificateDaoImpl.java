5245dde140356ae2d50430c93351349ec411047d
Peachex <klevolex@gmail.com>
2021-06-10 12:46:57 +0300

    add function

--- repository/src/main/java/com/epam/esm/dao/impl/GiftCertificateDaoImpl.java
+++ repository/src/main/java/com/epam/esm/dao/impl/GiftCertificateDaoImpl.java
@@ -1,12 +1,20 @@
 package com.epam.esm.dao.impl;
 
 import com.epam.esm.dao.GiftCertificateDao;
+import com.epam.esm.dao.constant.SqlGiftCertificateQuery;
 import com.epam.esm.dto.GiftCertificate;
 import com.epam.esm.dao.mapper.GiftCertificateMapper;
+import com.epam.esm.dto.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -25,8 +33,31 @@ public class GiftCertificateDaoImpl implements GiftCertificateDao<GiftCertificat
     }
 
     @Override
-    public boolean add(GiftCertificate giftCertificate) {
-        return false;
+    @Transactional
+    public boolean insert(GiftCertificate giftCertificate) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        boolean isQuerySuccess = template.update(con -> {
+            PreparedStatement ps = con.prepareStatement(SqlGiftCertificateQuery.INSERT_GIFT_CERTIFICATE,
+                    Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, giftCertificate.getName());
+            ps.setString(2, giftCertificate.getDescription());
+            ps.setBigDecimal(3, giftCertificate.getPrice());
+            ps.setInt(4, giftCertificate.getDuration());
+            ps.setTimestamp(5, Timestamp.valueOf(giftCertificate.getCreateDate()));
+            return ps;
+        }, keyHolder) == 1;
+        return (isQuerySuccess && updateTag(giftCertificate, keyHolder));
+    }
+
+    public boolean updateTag(GiftCertificate giftCertificate, KeyHolder keyHolder) {
+        List<Tag> tags = giftCertificate.getTags();
+        boolean result = false;
+        if (keyHolder.getKey() != null && tags != null && !tags.isEmpty()) {
+            result = tags.stream()
+                    .allMatch(t -> template.update(SqlGiftCertificateQuery.UPDATE_GIFT_CERTIFICATE_TAG,
+                            keyHolder.getKey().longValue(), t.getId()) == 1);
+        }
+        return result;
     }
 
     @Override
