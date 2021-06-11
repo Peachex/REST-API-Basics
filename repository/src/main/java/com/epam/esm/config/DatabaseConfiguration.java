6f0ca2611d86fe1592e783a531eedf87942063d3
Peachex <klevolex@gmail.com>
2021-06-11 19:47:42 +0300

    add dev and prod configuration

--- repository/src/main/java/com/epam/esm/config/DatabaseConfiguration.java
+++ repository/src/main/java/com/epam/esm/config/DatabaseConfiguration.java
@@ -3,8 +3,10 @@ package com.epam.esm.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -18,10 +20,13 @@ public class DatabaseConfiguration {
     private static final String DATABASE_URL = "spring.datasource.url";
     private static final String DATABASE_USERNAME = "spring.datasource.username";
     private static final String DATABASE_Password = "spring.datasource.password";
+    private static final String CREATE_DATABASE_SCRIPT = "classpath:script/schema.sql";
+    private static final String FILL_DATABASE_WITH_DATA_SCRIPT = "classpath:script/data.sql";
 
+    @Profile("prod")
     @Bean
     public DataSource dataSource() throws IOException {
-        Properties properties = new Properties();
+        Properties properties = new Properties(); //fixme add connection pool
         properties.load(DatabaseConfiguration.class.getResourceAsStream(DATABASE_PROPERTY_FILE_PATH));
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(properties.getProperty(DATABASE_DRIVER_CLASS_NAME));
@@ -31,8 +36,13 @@ public class DatabaseConfiguration {
         return dataSource;
     }
 
+    @Profile("dev")
     @Bean
-    public JdbcTemplate jdbcTemplate() throws IOException {
-        return new JdbcTemplate(dataSource());
+    public DataSource embeddedDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(CREATE_DATABASE_SCRIPT)
+                .addScript(FILL_DATABASE_WITH_DATA_SCRIPT)
+                .build();
     }
 }
