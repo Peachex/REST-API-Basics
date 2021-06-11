7dccc7026f66144d9a5176b5feaaaf6820186085
Peachex <klevolex@gmail.com>
2021-06-11 17:57:37 +0300

    add query

--- repository/src/main/java/com/epam/esm/dao/constant/SqlGiftCertificateQuery.java
+++ repository/src/main/java/com/epam/esm/dao/constant/SqlGiftCertificateQuery.java
@@ -21,6 +21,16 @@ public class SqlGiftCertificateQuery {
     public static final String SQL_UPDATE_CERTIFICATE_BY_ID = "UPDATE gift_certificates SET certificate_name = ?," +
             " description = ?, price = ?, duration = ?, last_update_date = ? WHERE gift_certificate_id = ?;";
 
+    public static final String SQL_SELECT_ALL_CERTIFICATES = "SELECT gift_certificate_id, certificate_name," +
+            " description, price, duration, create_date, last_update_date, tag_id, tag_name FROM gift_certificates" +
+            " LEFT JOIN gift_certificates_tags ON gift_certificate_id = gift_certificate_id_fk" +
+            " LEFT JOIN tags ON tag_id = tag_id_fk;";
+
+    public static final String SQL_SELECT_CERTIFICATES_WITH_TAGS = "SELECT gift_certificate_id, certificate_name," +
+            " description, price, duration, create_date, last_update_date, tag_id, tag_name FROM gift_certificates" +
+            " RIGHT JOIN gift_certificates_tags ON gift_certificate_id = gift_certificate_id_fk" +
+            " LEFT JOIN tags ON tag_id = tag_id_fk;";
+
     private SqlGiftCertificateQuery() {
     }
 }
