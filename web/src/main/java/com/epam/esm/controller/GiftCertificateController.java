386f518dc80c46a65b0864aa5991ffa7314fb8d4
Peachex <klevolex@gmail.com>
2021-06-11 09:18:56 +0300

    add function

--- web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
+++ web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -5,7 +5,9 @@ import com.epam.esm.service.GiftCertificateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,8 +31,20 @@ public class GiftCertificateController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<String> createTag(@RequestBody GiftCertificate giftCertificate) {
+    public ResponseEntity<String> createGiftCertificate(@RequestBody GiftCertificate giftCertificate) {
         service.insert(giftCertificate);
         return ResponseEntity.status(HttpStatus.CREATED).body("Certificate created successfully");
+    }
+
+    @GetMapping("/{id}")
+    public GiftCertificate findCertificateById(@PathVariable String id) {
+        return service.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteGiftCertificate(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Gift certificate deleted successfully" +
+                " (id = " + id + ")");
     }
 }
