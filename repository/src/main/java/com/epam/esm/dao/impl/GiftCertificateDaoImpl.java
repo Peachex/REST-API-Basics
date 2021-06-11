9839ae8268c98c53df1524ff03324982e0106831
Peachex <klevolex@gmail.com>
2021-06-11 19:48:04 +0300

    change constructor parameter

--- repository/src/main/java/com/epam/esm/dao/impl/GiftCertificateDaoImpl.java
+++ repository/src/main/java/com/epam/esm/dao/impl/GiftCertificateDaoImpl.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.Timestamp;
@@ -27,8 +28,8 @@ public class GiftCertificateDaoImpl implements GiftCertificateDao<GiftCertificat
     private final SqlQueryCreator queryCreator;
 
     @Autowired
-    public GiftCertificateDaoImpl(JdbcTemplate template, GiftCertificateMapper mapper, SqlQueryCreator queryCreator) {
-        this.template = template;
+    public GiftCertificateDaoImpl(DataSource dataSource, GiftCertificateMapper mapper, SqlQueryCreator queryCreator) {
+        this.template = new JdbcTemplate(dataSource);
         this.mapper = mapper;
         this.queryCreator = queryCreator;
     }
