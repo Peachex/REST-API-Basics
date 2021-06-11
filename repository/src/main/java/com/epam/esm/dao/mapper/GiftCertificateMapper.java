394ce1dbef6bff2bd61a11f8381f4d8016ecf009
Peachex <klevolex@gmail.com>
2021-06-11 09:18:01 +0300

    fix bug with null pointer exception

--- repository/src/main/java/com/epam/esm/dao/mapper/GiftCertificateMapper.java
+++ repository/src/main/java/com/epam/esm/dao/mapper/GiftCertificateMapper.java
@@ -20,25 +20,26 @@ public class GiftCertificateMapper implements ResultSetExtractor<List<GiftCertif
     @Override
     public List<GiftCertificate> extractData(ResultSet rs) throws SQLException, DataAccessException {
         List<GiftCertificate> giftCertificates = new ArrayList<>();
-        rs.next();
-        while (!rs.isAfterLast()) {
-            long giftCertificateId = rs.getLong(SqlGiftCertificateColumnName.GIFT_CERTIFICATE_ID);
-            String giftCertificateName = rs.getString(SqlGiftCertificateColumnName.NAME);
-            String description = rs.getString(SqlGiftCertificateColumnName.DESCRIPTION);
-            BigDecimal price = rs.getBigDecimal(SqlGiftCertificateColumnName.PRICE);
-            int duration = rs.getInt(SqlGiftCertificateColumnName.DURATION);
-            LocalDateTime createDate = rs.getObject(SqlGiftCertificateColumnName.CREATE_DATE, LocalDateTime.class);
-            LocalDateTime lastUpdateDate = rs.getObject(SqlGiftCertificateColumnName.LAST_UPDATE_DATE, LocalDateTime.class);
+        if (rs.next()) {
+            while (!rs.isAfterLast()) {
+                long giftCertificateId = rs.getLong(SqlGiftCertificateColumnName.GIFT_CERTIFICATE_ID);
+                String giftCertificateName = rs.getString(SqlGiftCertificateColumnName.NAME);
+                String description = rs.getString(SqlGiftCertificateColumnName.DESCRIPTION);
+                BigDecimal price = rs.getBigDecimal(SqlGiftCertificateColumnName.PRICE);
+                int duration = rs.getInt(SqlGiftCertificateColumnName.DURATION);
+                LocalDateTime createDate = rs.getObject(SqlGiftCertificateColumnName.CREATE_DATE, LocalDateTime.class);
+                LocalDateTime lastUpdateDate = rs.getObject(SqlGiftCertificateColumnName.LAST_UPDATE_DATE, LocalDateTime.class);
 
-            List<Tag> tags = new ArrayList<>();
-            do {
-                long tagId = rs.getLong(SqlTagColumnName.TAG_ID);
-                String tagName = rs.getString(SqlTagColumnName.TAG_NAME);
-                if (tagId != 0 && tagName != null) {
-                    tags.add(new Tag(tagId, tagName));
-                }
-            } while (rs.next() && rs.getLong(SqlGiftCertificateColumnName.GIFT_CERTIFICATE_ID) == giftCertificateId);
-            giftCertificates.add(new GiftCertificate(giftCertificateId, giftCertificateName, description, price, duration, createDate, lastUpdateDate, tags));
+                List<Tag> tags = new ArrayList<>();
+                do {
+                    long tagId = rs.getLong(SqlTagColumnName.TAG_ID);
+                    String tagName = rs.getString(SqlTagColumnName.TAG_NAME);
+                    if (tagId != 0 && tagName != null) {
+                        tags.add(new Tag(tagId, tagName));
+                    }
+                } while (rs.next() && rs.getLong(SqlGiftCertificateColumnName.GIFT_CERTIFICATE_ID) == giftCertificateId);
+                giftCertificates.add(new GiftCertificate(giftCertificateId, giftCertificateName, description, price, duration, createDate, lastUpdateDate, tags));
+            }
         }
         return giftCertificates;
     }
