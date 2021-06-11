aa0960b41a28c0b10d3b7e8d25d80835944d08d3
Peachex <klevolex@gmail.com>
2021-06-11 11:04:14 +0300

    add query

--- repository/src/main/java/com/epam/esm/dao/constant/SqlTagQuery.java
+++ repository/src/main/java/com/epam/esm/dao/constant/SqlTagQuery.java
@@ -3,6 +3,9 @@ package com.epam.esm.dao.constant;
 public class SqlTagQuery {
     public static final String SQL_SELECT_ALL_TAGS = "SELECT tag_id, tag_name FROM tags;";
 
+    public static final String SQL_SELECT_TAGS_CONNECTED_WITH_CERTIFICATE = "SELECT tag_id, tag_name FROM" +
+            " gift_certificates_tags JOIN tags ON tag_id_fk = tag_id WHERE gift_certificate_id_fk = ?;";
+
     public static final String SQL_SELECT_TAG_BY_ID = "SELECT tag_id, tag_name FROM tags WHERE tag_id = ?;";
 
     public static final String SQL_SELECT_TAG_BY_NAME = "SELECT tag_id, tag_name FROM tags WHERE tag_name = ?;";
