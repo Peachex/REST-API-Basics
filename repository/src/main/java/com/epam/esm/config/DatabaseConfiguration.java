aa9ad30e374065446f139e544d535e7356168e28
Peachex <klevolex@gmail.com>
2021-06-12 13:01:26 +0300

    add connection pool

--- repository/src/main/java/com/epam/esm/config/DatabaseConfiguration.java
+++ repository/src/main/java/com/epam/esm/config/DatabaseConfiguration.java
@@ -1,10 +1,9 @@
 package com.epam.esm.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -13,26 +12,27 @@ import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
-@ComponentScan("com.epam.esm")
 public class DatabaseConfiguration {
     private static final String DATABASE_PROPERTY_FILE_PATH = "/property/database.properties";
     private static final String DATABASE_DRIVER_CLASS_NAME = "spring.datasource.driver-class-name";
     private static final String DATABASE_URL = "spring.datasource.url";
     private static final String DATABASE_USERNAME = "spring.datasource.username";
-    private static final String DATABASE_Password = "spring.datasource.password";
+    private static final String DATABASE_PASSWORD = "spring.datasource.password";
+    private static final String DATABASE_POOL_MAX_SIZE = "spring.datasource.poolMaxSize";
     private static final String CREATE_DATABASE_SCRIPT = "classpath:script/schema.sql";
     private static final String FILL_DATABASE_WITH_DATA_SCRIPT = "classpath:script/data.sql";
 
     @Profile("prod")
     @Bean
     public DataSource dataSource() throws IOException {
-        Properties properties = new Properties(); //fixme add connection pool
+        Properties properties = new Properties();
         properties.load(DatabaseConfiguration.class.getResourceAsStream(DATABASE_PROPERTY_FILE_PATH));
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        HikariDataSource dataSource = new HikariDataSource();
         dataSource.setDriverClassName(properties.getProperty(DATABASE_DRIVER_CLASS_NAME));
-        dataSource.setUrl(properties.getProperty(DATABASE_URL));
+        dataSource.setJdbcUrl(properties.getProperty(DATABASE_URL));
         dataSource.setUsername(properties.getProperty(DATABASE_USERNAME));
-        dataSource.setPassword(properties.getProperty(DATABASE_Password));
+        dataSource.setPassword(properties.getProperty(DATABASE_PASSWORD));
+        dataSource.setMaximumPoolSize(Integer.parseInt(properties.getProperty(DATABASE_POOL_MAX_SIZE)));
         return dataSource;
     }
 
